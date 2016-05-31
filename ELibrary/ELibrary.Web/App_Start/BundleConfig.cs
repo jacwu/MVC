e53247e1bc4048a1c37611d49b8b9f4831f633f4
jacwu <wsjeep29@hotmail.com>
2016-05-31 21:59:39 +0800

    add js and css, add layout

---
+++ ELibrary/ELibrary.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ELibrary.Web
+{
+    public class BundleConfig
+    {            
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery.validate.unobtrusive.js",
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                        "~/css/bootstrap.css"));
+        }
+    }
+
+}
--- ELibrary/ELibrary.Web/Global.asax.cs
+++ ELibrary/ELibrary.Web/Global.asax.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ELibrary.Web
@@ -15,6 +16,7 @@ namespace ELibrary.Web
 
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
             Bootstrapper.Run();
         }
