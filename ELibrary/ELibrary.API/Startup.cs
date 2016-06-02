932dd303edb440659d93fb14cfe1f1ab55f2264b
xinjinmsn <xinjin@msn.com>
2016-06-02 22:10:14 +0800

    API: Remove Global.asax, Add Startup Class

---
+++ ELibrary/ELibrary.API/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ELibrary.API.Startup))]
+
+namespace ELibrary.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            HttpConfiguration config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+
+        }
+    }
+}
