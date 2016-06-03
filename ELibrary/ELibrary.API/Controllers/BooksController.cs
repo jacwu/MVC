81cafd40eb3f9b557a9a5501ddaf2807b773ed28
xinjinmsn <xinjin@msn.com>
2016-06-03 19:12:27 +0800

    API: Implement a faked BooksController

---
+++ ELibrary/ELibrary.API/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ELibrary.API.Controllers
+{
+    [Route("api/library/books/{bookId?}", Name ="Books")]
+    public class BooksController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+    }
+}
