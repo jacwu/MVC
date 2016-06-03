b64d4ec9a26c5f543c44d9ac400278ac9f2e7b59
xinjinmsn <xinjin@msn.com>
2016-06-03 16:08:42 +0800

    API: Support JSON CamelCase

--- ELibrary/ELibrary.API/App_Start/WebApiConfig.cs
+++ ELibrary/ELibrary.API/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace ELibrary.API
@@ -14,11 +16,13 @@ namespace ELibrary.API
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            //config.Routes.MapHttpRoute(
-            //    name: "DefaultApi",
-            //    routeTemplate: "api/{controller}/{id}",
-            //    defaults: new { id = RouteParameter.Optional }
-            //);
+            //Support JSON CamelCase
+            var jsonFormatter = config.Formatters.JsonFormatter;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            
+
         }
     }
 }
